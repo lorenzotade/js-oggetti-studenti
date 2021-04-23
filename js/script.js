09c0a95d17b1aad627821cc25f1e91b7e3a06028
lorenzotade <lorenzo.tade@gmail.com>
2021-04-23 16:54:33 +0200

    funzione bottone

--- js/script.js
+++ js/script.js
@@ -35,48 +35,48 @@ var arrStudents = [
   }
 ];
 
-// chiamo la funzione per inserire oggetto
-// con target il mio array di oggetti
-//insertStudent(arrStudents);
-
-// con questo ciclo for..of + for..in stampo a 
-// video tutti i valori di ogni singolo 
-// oggetto nell'array
-/* for (var student of arrStudents) {
-  for (var key in student) {
-    console.log(student[key]);
-  }
-  console.log('--------------');
-}
-
-console.log("===============\nsecondo ciclo for\n==============="); */
-
-
-
-// con questo ciclo for..of stampo a video
-// in forma umana i valori di ogni singolo
-// oggetto nell'array.
-for (var i = 0; i < arrStudents.length; i++) {
-  var htmlName = '<li>Nome: ' + student['firstName'] + '</li><br>';
-  var htmlSurname = '<li>Cognome: ' + student['lastName'] + '</li><br>';
-  var htmlAge = '<li>Anni: ' + student['age'] + '</li><br>';
-  var studentList = document.getElementById('studentList');
-  studentList.innerHTML += studentList + htmlName + htmlSurname + htmlAge;
-  /* console.log("Nome: " + student['firstName']);
-  console.log("Cognome: " + student['lastName']);
-  console.log("Anni: " + student['age']);
-  console.log('--------------'); */
-}
-
- // funzione per inserire uno studente nell'array.
- // questa funzione pusha un oggetto le cui proprietà
- // sono definite dai prompt inseriti dall'utente.
-function insertStudent(target) {
-  target.push(
+// metto un event listener al bottone #add che
+$(document).on('click', '#add', function(){
+  arrStudents.push(
     {
       firstName: prompt("Inserisci il tuo nome:"),
       lastName: prompt("Inserisci il tuo cognome:"),
       age: parseInt(prompt("Inserisci la tua età:"))
     }
   );
+})
+//insertStudent(arrStudents);
+
+// con questo ciclo for..of + for..in stampo a 
+// video tutti i valori di ogni singolo 
+// oggetto nell'array
+for (var student of arrStudents) {
+  for (var key in student) {
+    console.log(student[key]);
+  }
+  console.log('--------------');
 }
+
+console.log("===============\nsecondo ciclo for\n===============");
+
+
+
+// con questo ciclo for..of stampo a video
+// in forma umana i valori di ogni singolo
+// oggetto nell'array.
+for (var student of arrStudents) {
+  // stampa in html
+  var htmlMsg = '<li><strong>Nome:</strong> ' + student['firstName'] + '<br><strong>Cognome:</strong> ' + student['lastName'] + '<br><strong>Anni:</strong> ' + student['age'] + '</li>';
+  var studentList = document.getElementById('studentList');
+  studentList.innerHTML += htmlMsg;
+
+  // stampa a console
+  console.log("Nome: " + student['firstName']);
+  console.log("Cognome: " + student['lastName']);
+  console.log("Anni: " + student['age']);
+  console.log('--------------');
+}
+
+ // funzione per inserire uno studente nell'array.
+ // questa funzione pusha un oggetto le cui proprietà
+ // sono definite dai prompt inseriti dall'utente.

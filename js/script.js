5922c697bac23336df12cfbcc311925b2ccc17ad
lorenzotade <lorenzo.tade@gmail.com>
2021-04-23 18:03:00 +0200

    terminato

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var objStudent = {
   lastName: 'Tadè',
   age: 28
 };
+
 // ciclo nell'oggetto per stampare le proprietà
 // e i valori dell'oggetto creato
 for (var key in objStudent) {
@@ -54,8 +55,8 @@ printArray();
 // metto un event listener al bottone #add che
 // richiama una funzione per chiedere all'utente
 // dei dati pushando il tutto nell'array.
-// Successivamente richiama la funzione printArray()
-// per ristampare l'array aggiornato col push
+// Successivamente richiama la funzione printNew()
+// per stampare l'elemento aggiunto dall'utente
 $(document).on('click', '#add', function(){
   arrStudents.push(
     {
@@ -67,6 +68,7 @@ $(document).on('click', '#add', function(){
   printNew();
 });
 
+/* FUNZIONI */
 
  // funzione per stampare in console e in html l'array.
 function printArray() {
@@ -74,11 +76,10 @@ function printArray() {
   // in forma umana i valori di ogni singolo
   // oggetto nell'array.
   for (var student of arrStudents) {
-    // stampa in html
     var htmlMsg = '<li><strong>Nome:</strong> ' + student['firstName'] + '<br><strong>Cognome:</strong> ' + student['lastName'] + '<br><strong>Anni:</strong> ' + student['age'] + '</li>';
+    // stampa in html
     var studentList = document.getElementById('studentList');
     studentList.innerHTML += htmlMsg;
-
     // stampa a console
     console.log("Nome: " + student['firstName']);
     console.log("Cognome: " + student['lastName']);
@@ -87,11 +88,11 @@ function printArray() {
   }
 }
 
+// funzione per stampare solo l'ultimo elemento
+// dell'array (quello aggiunto dall'utente)
 function printNew() {
-  var limit = arrStudents.length;
-  for (var i = limit; i <= arrStudents[limit]; i++) {
-    var htmlMsg = '<li><strong>Nome:</strong> ' + student['firstName'] + '<br><strong>Cognome:</strong> ' + student['lastName'] + '<br><strong>Anni:</strong> ' + student['age'] + '</li>';
-    var studentList = document.getElementById('studentList');
-    studentList.innerHTML += htmlMsg;
-  }
+  var student = arrStudents[(arrStudents.length - 1)];
+  var htmlMsg = '<li><strong>Nome:</strong> ' + student['firstName'] + '<br><strong>Cognome:</strong> ' + student['lastName'] + '<br><strong>Anni:</strong> ' + student['age'] + '</li>';
+  var studentList = document.getElementById('studentList');
+  studentList.innerHTML += htmlMsg;
 }
